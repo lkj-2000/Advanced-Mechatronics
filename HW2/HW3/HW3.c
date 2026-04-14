dc916f6779a2cd23a0dee2b313296edf0bc4eb2a
Anisha Phade <anishaphade05@gmail.com>
2026-04-14 14:31:44 -0500

    working i2c

--- HW2/HW3/HW3.c
+++ HW2/HW3/HW3.c
@@ -8,15 +8,14 @@
 #define I2C_PORT i2c0
 #define I2C_SDA 8
 #define I2C_SCL 9
-#define ADDR 0b1000000
+#define ADDR 0b0100000 // 0x20
 #define OLAT_ADDR 0b00001010 // 0Ah
+#define GPIO_ADDR 0b00001001 // 09h
 #define IODIR_ADDR 0b00000000 // 00h
-uint8_t buf[2];
 
-void setPin(unsigned char address, unsigned char reg, unsigned char value){
-    buf[0] = reg;
-    buf[1] = value;
-    i2c_write_blocking(I2C_PORT, address, buf, 2, false);
+void setPin(unsigned char reg, unsigned char value){
+    uint8_t buf[2] = {reg, value};
+    i2c_write_blocking(I2C_PORT, ADDR, buf, 2, false);
 }
 
 void chip_initialization(){
@@ -31,30 +30,37 @@ void chip_initialization(){
     //gpio_pull_up(I2C_SCL);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    setPin(ADDR, IODIR_ADDR, 0b01111111); // set GP7 as output and GP0 as input (write an unsigned char to IODIR to set direction of each pin)
+    setPin(IODIR_ADDR, 0b01111111); // set GP7 as output and GP0 as input (write an unsigned char to IODIR to set direction of each pin)
 }
 
-unsigned char readPin(unsigned char address, unsigned char reg){
-    i2c_write_blocking(I2C_PORT, address, &reg, 1, true);  // true to keep host control of bus
-    i2c_read_blocking(I2C_PORT, address, &buf[1], 1, false);  // false - finished with bus
-    return buf[1];
+unsigned char readPin(unsigned char reg){
+    i2c_write_blocking(I2C_PORT, ADDR, &reg, 1, true);  // true to keep host control of bus
+    uint8_t value;
+    i2c_read_blocking(I2C_PORT, ADDR, &value, 1, false);  // false - finished with bus
+    return value;
 }
 
-int main()
+void led_flash(){
+    setPin(GPIO_ADDR, 0b10000000);  // set bit 0 to 1 (turn pin on)
+    sleep_ms(1000);
+    setPin(GPIO_ADDR, 0b00000000);   // set bits to OLAT to turn pins on or off
+    sleep_ms(1000);
+}
+
+int main()  
 {
     chip_initialization();
 
-    // read from GPIO to know if button is pushed
     while (true) {
-        setPin(ADDR, OLAT_ADDR, 0b10000000);  // set bit 0 to 1 (turn pin on)
-        sleep_ms(1000);
-        setPin(ADDR, OLAT_ADDR, 0b00000000);   // set bits to OLAT to turn pins on or off
-        sleep_ms(1000);
-        // if readPin(ADDR, ){
-        //     setPin(ADDR, OLAT_ADDR, 0b00000001);  // set bit 0 to 1 (turn pin on)
-        //  }
-        // else{
-        //     setPin(ADDR, OLAT_ADDR, 0b00000000);   // set bits to OLAT to turn pins on or off           
-        // }
+        // led_flash();
+
+        uint8_t gpio_current = readPin(GPIO_ADDR);  // read from GPIO to know if button is pushed
+        // if button pushed (low), need to set high
+        if ((gpio_current & 0b00000001) == 0){
+            setPin(GPIO_ADDR, gpio_current | 0b10000000);  // set bit 0 to 1 (turn pin on)
+        }
+        else{
+            setPin(GPIO_ADDR, gpio_current & 0b01111111);
+        }
     }
 }
